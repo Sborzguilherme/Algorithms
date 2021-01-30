f9c31538cbbc41e2f97deccc089ec3728b99da14
Guilherme Sborz <sborzguilherme@gmail.com>
2021-01-30 09:29:42 -0300

    Count split inversions - divide and conquer algorithms

---
+++ src/algorithms_stanford_part_I/countSplitInversion.py
@@ -0,0 +1,33 @@
+def mergeAndCountSplitInv (arrL, arrR, n):
+  sortedArray = []
+  
+  while(len(arrL) > 0 and len(arrR) > 0):
+    if(arrR[0] < arrL[0]): 
+      sortedArray.append(arrR.pop(0))
+      if(len(arrL) > 0):
+        n+=len(arrL)
+    else:
+      sortedArray.append(arrL.pop(0))
+  
+  while(len(arrL) > 0):
+    sortedArray.append(arrL.pop(0))
+
+  while(len(arrR) > 0):
+    sortedArray.append(arrR.pop(0))
+
+  return [sortedArray, n]
+
+def countSplitInv (arr):
+  if(len(arr) <= 1) :
+    return [arr,0]
+
+  half = len(arr)//2
+  [left_array,x] = countSplitInv(arr[:half])
+  [right_array,y] = countSplitInv(arr[half:])
+
+  return mergeAndCountSplitInv(left_array, right_array, x+y)
+
+
+test = [6,5,4,3,2,1]
+[arr,result] = countSplitInv(test)
+print('Split Inversions {}'.format(result))
